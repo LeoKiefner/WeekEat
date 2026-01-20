06f062db9b3740b294ea7eea35fa8585599a3a7b
Léo Kiefner <leokiefner.pro@gmail.com>
2026-01-20 23:06:08 +0100

    feat(stats) : Pagespeed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 import { SessionProvider } from "@/components/providers/session-provider"
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="fr">
       <body className={inter.className}>
         <SessionProvider>{children}</SessionProvider>
+        <SpeedInsights />
       </body>
     </html>
   )
